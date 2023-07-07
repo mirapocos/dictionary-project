b052ce6224c61103e2504066bcdd0b8d285805ec
mirapocos <112176776+mirapocos@users.noreply.github.com>
2023-07-07 19:08:43 +0100

    Adding the Search Engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import logo from "./water-color-flowers.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Mira Pocos</small>
+        </footer>
+      </div>
     </div>
   );
 }
