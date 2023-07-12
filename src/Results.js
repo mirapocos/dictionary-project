7005e32518d03dd12d3650145342c454c2d0367c
mirapocos <112176776+mirapocos@users.noreply.github.com>
2023-07-12 19:25:04 +0100

    Fixed the access to the API properties

--- src/Results.js
+++ src/Results.js
@@ -2,11 +2,11 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  if (props.sesults) {
+  console.log({ props });
+  if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.meaning.map(function (meaning, index) {
+        {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
               <Meaning meaning={meaning} />
